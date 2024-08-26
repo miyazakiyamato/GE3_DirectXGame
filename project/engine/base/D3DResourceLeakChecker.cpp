91e5123065e0c622fe2ca0b80e039d405fa01a2f
miyazakiyamato <146906497+miyazakiyamato@users.noreply.github.com>
2024-08-26 11:56:27 +0900

    07_03_確認課題

--- project/engine/base/D3DResourceLeakChecker.cpp
+++ project/engine/base/D3DResourceLeakChecker.cpp
@@ -1,4 +1,4 @@
-﻿#include "D3DResourceLeakChecker.h"
+#include "D3DResourceLeakChecker.h"
 #include <dxgidebug.h>
 #include <d3d12.h>
 #include <dxgi1_6.h>
@@ -7,6 +7,7 @@
 using namespace Microsoft::WRL;
 
 D3DResourceLeakChecker::~D3DResourceLeakChecker() {
+#ifdef DEBUG
 	//リソースリークチェック
 	ComPtr<IDXGIDebug1> debug;
 	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
@@ -14,4 +15,5 @@ D3DResourceLeakChecker::~D3DResourceLeakChecker() {
 		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
 		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
 	}
+#endif // DEBUG
 }
