61a67094e15cd949fbb4fdf97ed9b3c02dbe49b3
miyazakiyamato <146906497+miyazakiyamato@users.noreply.github.com>
2025-07-04 18:58:28 +0900

    01_06

--- project/resources/tools/level_editor.py
+++ project/resources/tools/level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 # ブレンダーの登録するアドオン情報
 bl_info = {
     "name": "レベルエディタ",
@@ -15,6 +16,35 @@ bl_info = {
 # メニュー項目描画
 def draw_menu_manual(self, context):
     self.layout.operator("wm.url_open_preset", text = "Manual", icon = 'HELP')
+#オペレータ シーン出力
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"     #Blenderがクラスを識別する為の固有の文字列
+    bl_label = "シーン出力"                            #メニューのラベルとして表示される文字列
+    bl_description = "シーン情報をExportします"         #説明表示用の文字列
+    #メニューを実行したときに呼ばれるコールバック関数
+    def execute(self,context):
+        print("シーン情報をExportします")
+        #シーン内の全オブジェクトについて
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)
+            #ローカルトランスフォーム行列から平行移動、回転、スケーリングを抽出
+            trans,rot,scale = object.matrix_local.decompose()
+            rot = rot.to_euler()#Quaternion から Euler に変換
+            #ラジアンから度数法に変換
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+            #トランスフォーム情報を表示
+            print("Trans(%f,%f,%f)" % (trans.x,trans.y,trans.z))
+            print("Rot(%f,%f,%f)" % (rot.x,rot.y,rot.z))
+            print("Scale(%f,%f,%f)" % (scale.x,scale.y,scale.z))
+            #親オブジェクトの名前を表示
+            if object.parent:
+                print("Parent:" + object.parent.name)
+            print()
+        print("シーン情報をExportしました")
+        self.report({'INFO'},"シーン情報をExportしました")
+        return {'FINISHED'}
 #オペレータ 頂点を伸ばす
 class MYADDON_OT_stretch_vertex(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_stretch_vertex"     #Blenderがクラスを識別する為の固有の文字列
@@ -47,6 +77,8 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     # サブメニューの描画
     def draw(self,context):
         # トップバーの「エディターメニュー」に項目（オペレーター）を追加
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname,
+                             text = MYADDON_OT_export_scene.bl_label)
         self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname,
                              text = MYADDON_OT_stretch_vertex.bl_label)
         self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname,
@@ -58,6 +90,7 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 #Blenderに登録するクラスリスト
 classes = (
+    MYADDON_OT_export_scene,
     MYADDON_OT_stretch_vertex,
     MYADDON_OT_create_ico_sphere,
     TOPBAR_MT_my_menu,
